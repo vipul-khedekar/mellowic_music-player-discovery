2c339bd832dcfb0aa5fd8fcf4cef58cda77a0610
vipul-khedekar <3kvipul@gmail.com>
2022-10-05 21:19:59 +0530

    Update: Constants for sizes and hex color codes.

--- src/assets/common.js
+++ src/assets/common.js
@@ -2,3 +2,32 @@ export const deviceSizes = {
   tabletScreen: `only screen and (max-width: 1023px)`,
   mobileScreen: `only screen and (max-width: 767px)`,
 };
+
+export const sizes = {
+  xxxLarge: `2rem`,
+  xxLarge: `1.5rem`,
+  xLarge: `1.25`,
+  large: `1.125rem`,
+  medium: `1rem`,
+  small: `0.875rem`,
+  xSmall: `0.75rem`,
+};
+
+export const miscSizes = {
+  xxxLarge: `2rem`,
+  xxLarge: `1.5rem`,
+  xLarge: `1rem`,
+  large: `0.75rem`,
+  medium: `0.5rem`,
+  small: `0.125rem`,
+};
+
+export const colors = {
+  darkBerry: `#673653`,
+  lightBerry: `#ae606d`,
+  darkPeach: `#de8274`,
+  lightPeach: `#fdac89`,
+  orangish: `#ed6a56`,
+  yellowish: `#fec252`,
+  greenish: `#42b49f`,
+};
