98fcc146386af547be541cf342b60ab7bc3b2229
vipul-khedekar <3kvipul@gmail.com>
2022-10-06 17:58:01 +0530

    Update: Setup for RTK. Service setup to fetch data from shazam core. Provided the store to the main app.

---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import { shazamCoreApi } from "./services/shazamCore";
+
+export const store = configureStore({
+  reducer: {
+    [shazamCoreApi.reducerPath]: shazamCoreApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(shazamCoreApi.middleware),
+});
