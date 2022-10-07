9e336d3f223b6b0a57ff775f39f87d8ae9a66bbc
vipul-khedekar <3kvipul@gmail.com>
2022-10-07 17:18:04 +0530

    Update: More text options by size. Fix: Use of paragraph tag.

--- src/styles/Text.js
+++ src/styles/Text.js
@@ -2,7 +2,20 @@ import styled from "styled-components";
 
 import { sizes } from "../assets/common";
 
-export const BigText = styled.h2`
+export const VeryBigText = styled.p`
   font-weight: bold;
-  font-size: ${sizes.xlarge};
+  font-size: ${sizes.xLarge};
+`;
+
+export const BigText = styled.p`
+  font-weight: bold;
+  font-size: ${sizes.large};
+`;
+
+export const MidText = styled.p`
+  font-size: ${sizes.medium};
+`;
+
+export const SmallText = styled.p`
+  font-size: ${sizes.small};
 `;
