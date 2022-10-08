b321985fcfb5b24c61028c87db850a9509ce5165
vipul-khedekar <3kvipul@gmail.com>
2022-10-08 16:35:49 +0530

    Update: Artists area styled with dummy placeholders.

--- src/components/right-panel/ArtistsArea.jsx
+++ src/components/right-panel/ArtistsArea.jsx
@@ -1,5 +1,34 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode } from "swiper";
+import "swiper/css";
+import "swiper/css/free-mode";
+
+import { BigText, SmallText } from "../../styles/Text";
+import {
+  Container,
+  Heading,
+  LinkStyled,
+  List,
+} from "../../styles/components/ArtistAreaStyled";
+
 function ArtistsArea() {
-  return <div>ArtistsArea</div>;
+  return (
+    <Container>
+      <Heading>
+        <BigText>Top Artists</BigText>
+
+        <LinkStyled to={`/top-artists`}>
+          <SmallText>See more...</SmallText>
+        </LinkStyled>
+      </Heading>
+
+      <List>
+        {[1, 2, 3, 4, 5].map((artist) => {
+          return <p>{artist}</p>;
+        })}
+      </List>
+    </Container>
+  );
 }
 
 export default ArtistsArea;
---
+++ src/styles/components/ArtistAreaStyled.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+import { colors } from "../../assets/common";
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Heading = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const LinkStyled = styled(Link)`
+  color: ${colors.letters};
+  letter-spacing: 1px;
+  text-underline-offset: 3px;
+`;
+
+export const List = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
