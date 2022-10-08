aa1f91879e76adfac56088613e88887bc8f13e1a
vipul-khedekar <3kvipul@gmail.com>
2022-10-08 16:48:41 +0530

    Update: Charts area styled with dummy placeholders.

--- src/components/right-panel/ChartArea.jsx
+++ src/components/right-panel/ChartArea.jsx
@@ -1,5 +1,29 @@
+import { BigText, SmallText } from "../../styles/Text";
+import {
+  Box,
+  Heading,
+  LinkStyled,
+} from "../../styles/components/RightPanelStyled";
+import { List } from "../../styles/components/ChartAreaStyled";
+
 function ChartArea() {
-  return <div>ChartArea</div>;
+  return (
+    <Box>
+      <Heading>
+        <BigText>Top Charts</BigText>
+
+        <LinkStyled to={`/top-charts`}>
+          <SmallText>See more...</SmallText>
+        </LinkStyled>
+      </Heading>
+
+      <List>
+        {[1, 2, 3, 4, 5].map((song) => {
+          return <p>{song}</p>;
+        })}
+      </List>
+    </Box>
+  );
 }
 
 export default ChartArea;
