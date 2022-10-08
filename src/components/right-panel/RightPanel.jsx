7560296b29ec2af7473f023815f4bb510e38a3d3
vipul-khedekar <3kvipul@gmail.com>
2022-10-08 16:09:05 +0530

    Update: Right panel layout organized.

--- src/components/right-panel/RightPanel.jsx
+++ src/components/right-panel/RightPanel.jsx
@@ -1,13 +1,35 @@
+import { useSelector, useDispatch } from "react-redux";
+
+import { Container } from "../../styles/components/RightPanelStyled";
 import ChartArea from "./ChartArea";
 import ArtistsArea from "./ArtistsArea";
+import { PlayAndPause } from "../PlayAndPause";
+import { setActiveSong, playAndPause } from "../../store/features/player";
+import { useGetTopChartsQuery } from "../../store/services/shazamCore";
 
 function RightPanel() {
+  const dispatch = useDispatch();
+  const { setActiveSong, isPlaying } = useSelector((state) => state.player);
+
+  // const {data, isFetching, isError} = useGetTopChartsQuery()
+
+  // const topCharts = data?.substr(0, 5);
+
+  // function handlePlay() {
+  //   dispatch(playAndPause(true));
+  //   dispatch(setActiveSong({ data }));
+  // }
+
+  // function handlePause() {
+  //   dispatch(playAndPause(false));
+  // }
+
   return (
-    <div>
+    <Container>
       <ArtistsArea />
 
       <ChartArea />
-    </div>
+    </Container>
   );
 }
 
---
+++ src/styles/components/RightPanelStyled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 2rem;
+  padding: 1rem;
+`;
