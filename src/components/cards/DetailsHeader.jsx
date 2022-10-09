bcd8766d0bd36ba73af3fcb726910c346037efdb
vipul-khedekar <3kvipul@gmail.com>
2022-10-09 20:31:39 +0530

    Fix: Fallback default image.

--- src/components/cards/DetailsHeader.jsx
+++ src/components/cards/DetailsHeader.jsx
@@ -6,6 +6,8 @@ import {
 } from "../../styles/components/DetailsHeaderStyled";
 import { BigText, MidText, SmallText } from "../../styles/Text";
 
+import DefaultImage from "../../assets/default-image.jpg";
+
 function DetailsHeader(props) {
   const { songData } = props;
 
@@ -13,17 +15,20 @@ function DetailsHeader(props) {
     <Container>
       <Gradient />
 
-      <Image src={songData.images?.coverart} alt="cover-image" />
+      <Image
+        src={songData?.images?.coverart || DefaultImage}
+        alt="cover-image"
+      />
 
       <TextContainer>
-        <BigText style={{ whiteSpace: "nowrap" }}>{songData.title}</BigText>
+        <BigText style={{ whiteSpace: "nowrap" }}>{songData?.title}</BigText>
 
         <MidText style={{ margin: "-0.5rem 0", whiteSpace: "nowrap" }}>
-          {songData.subtitle}
+          {songData?.subtitle}
         </MidText>
 
         <SmallText style={{ whiteSpace: "nowrap" }}>
-          {songData.genres.primary}
+          {songData?.genres.primary}
         </SmallText>
       </TextContainer>
     </Container>
