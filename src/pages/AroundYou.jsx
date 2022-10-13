5e209fe770871be6bd2974a4a5c0f36ffef55c43
vipul-khedekar <3kvipul@gmail.com>
2022-10-13 19:26:49 +0530

    Update: Fetched and consumed data. Passed user location to fetch query based on it.

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,59 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
+
+import DancingBars from "../components/loaders/DancingBars";
+import ResultsError from "../components/ResultsError";
+import Song from "../components/cards/Song";
+
+import { useGetSongsByLocationQuery } from "../store/services/shazamCore";
+
+import { Container } from "../styles/pages/AroundYouStyled";
+import { VeryBigText } from "../styles/Text";
+import { DisplayList } from "../styles/DisplayList";
+
 function AroundYou() {
-  return <div>AroundYou</div>;
+  const [location, setLocation] = useState(``);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, isError } = useGetSongsByLocationQuery(location);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://geo.ipify.org/api/v2/country?apiKey=${process.env.REACT_APP_GEO_API_KEY}`
+      )
+      .then((res) => setLocation(res?.data?.location.country))
+      .catch((err) => console.log(err))
+      .finally(() => setIsLoading(false));
+    console.log(location);
+  }, [location]);
+
+  return (
+    <Container>
+      <VeryBigText>Around You</VeryBigText>
+
+      {isLoading && isFetching && <DancingBars />}
+
+      {isError && location !== `` && <ResultsError />}
+
+      <DisplayList>
+        {data?.map((song, i) => {
+          return (
+            <Song
+              key={song.key}
+              i={i}
+              song={song}
+              data={data}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+            />
+          );
+        })}
+      </DisplayList>
+    </Container>
+  );
 }
 
 export default AroundYou;
