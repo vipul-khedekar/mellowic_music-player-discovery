fbb6770e9099c4afc9e0c00ce10e6383681de873
vipul-khedekar <3kvipul@gmail.com>
2022-10-13 17:14:45 +0530

    Update: Fetched and consumed top charts data.

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -1,5 +1,44 @@
+import { useSelector } from "react-redux";
+
+import { VeryBigText } from "../styles/Text";
+import { DisplayList } from "../styles/DisplayList";
+import { Container } from "../styles/pages/TopChartsStyled";
+
+import { useGetTopChartsQuery } from "../store/services/shazamCore";
+
+import DancingBars from "../components/loaders/DancingBars";
+import ResultsError from "../components/ResultsError";
+import Song from "../components/cards/Song";
+
 function TopCharts() {
-  return <div>TopCharts</div>;
+  const { setActiveSong, isPlaying } = useSelector((state) => state.player);
+
+  const { data, isFetching, isError } = useGetTopChartsQuery();
+
+  return (
+    <Container>
+      <VeryBigText>Top Charts</VeryBigText>
+
+      <DisplayList>
+        {isFetching && <DancingBars />}
+
+        {isError && <ResultsError />}
+
+        {data?.map((song, i) => {
+          return (
+            <Song
+              key={song.key}
+              i={i}
+              song={song}
+              data={data}
+              isPlaying={isPlaying}
+              setActiveSong={setActiveSong}
+            />
+          );
+        })}
+      </DisplayList>
+    </Container>
+  );
 }
 
 export default TopCharts;
