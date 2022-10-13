5e3501b62bada2dc5423e2798f7012a252611ecd
vipul-khedekar <3kvipul@gmail.com>
2022-10-13 23:32:06 +0530

    Update: Fetched and consumed search results by query.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <MiddlePanel>
         <SearchBar />
 
-        {/* <Pages /> */}
+        <Pages />
       </MiddlePanel>
 
       <RightPanel />
--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,5 +1,47 @@
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import { Container } from "../styles/pages/SearchResultsStyled";
+import { VeryBigText } from "../styles/Text";
+import { DisplayList } from "../styles/DisplayList";
+
+import { useGetSongsAsSearchedQuery } from "../store/services/shazamCore";
+
+import ResultsError from "../components/ResultsError";
+import DancingBar from "../components/loaders/DancingBars";
+import Song from "../components/cards/Song";
+
 function SearchResults() {
-  return <div>SearchResults</div>;
+  const { searchQuery } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, isError } = useGetSongsAsSearchedQuery(searchQuery);
+
+  const results = data?.tracks?.hits.map((song) => song.track);
+
+  return (
+    <Container>
+      <VeryBigText>Search Results for: {searchQuery}</VeryBigText>
+
+      {isFetching && <DancingBar />}
+
+      {isError && <ResultsError />}
+
+      <DisplayList>
+        {results?.map((song, i) => {
+          return (
+            <Song
+              key={song.key}
+              i={i}
+              song={song}
+              data={data}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+            />
+          );
+        })}
+      </DisplayList>
+    </Container>
+  );
 }
 
 export default SearchResults;
