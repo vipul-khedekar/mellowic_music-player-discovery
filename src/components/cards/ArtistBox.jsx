a01732568bd3283c7362b429b60d6dc91c21a39e
vipul-khedekar <3kvipul@proton.me>
2022-12-17 21:52:04 +0530

    Fix: Added a condition to prevent artist prop from containing undefined data.

--- src/components/cards/ArtistBox.jsx
+++ src/components/cards/ArtistBox.jsx
@@ -5,11 +5,17 @@ function ArtistBox(props) {
   const { images, subtitle, artists } = props.artist;
 
   return (
-    <Container to={`/artists/${artists[0].adamid}`}>
-      <Image src={images?.background} alt="artist-headshot" />
+    <>
+      {artists ? (
+        <Container to={`/artists/${artists[0].adamid}`}>
+          <Image src={images?.background} alt="artist-headshot" />
 
-      <MidText>{subtitle}</MidText>
-    </Container>
+          <MidText>{subtitle}</MidText>
+        </Container>
+      ) : (
+        <></>
+      )}
+    </>
   );
 }
 
